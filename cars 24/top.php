789c6f10cf1cd5b30a8d706bb39e42fce56bd538
zas1998 <113234199+zas1998@users.noreply.github.com>
2022-09-10 14:15:21 +0530

    Add files via upload

---
+++ cars 24/top.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/fontAwesome.css">
+        <link rel="stylesheet" href="css/hero-slider.css">
+        <link rel="stylesheet" href="css/owl-carousel.css">
+        <link rel="stylesheet" href="css/style.css">
+
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+        <style type="text/css">
+            .wrap img{
+                margin-top: 15px;
+            }
+        </style>
+    </head>
+
+<body>
+ 
+    <div class="wrap">
+        <header id="header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <button id="primary-nav-button" type="button">Menu</button>
+                        <a href="index.html"><div class="logo">
+                            <img src="imeges/cars 24 logo.jpg" alt="Venue Logo" style="height: 70px;">
+                        </div></a>
+                        <nav id="primary-nav" class="dropdown cf">
+                            <ul class="dropdown menu">
+                                <li><a href="used car.php">Buy Used Car</a></li>
+                                <li><a href="sell used car.php">Sell Used Car</a></li>
+                            </ul>
+                        </nav><!-- / #primary-nav -->
+                    </div>
+                </div>
+            </div>
+        </header>
+    </div>
+</body>
+</html>
---
+++ cars 24/used car.php
@@ -0,0 +1,46 @@
+<?php include('top.php');
+ ?>
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<meta charset="utf-8">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1">
+ 	<title></title>
+ 	<style type="text/css">
+ 		.container input{
+ 			height: 70px;
+ 			width: 280px;
+ 			border-radius: 13px;
+ 			text-align: center;
+ 			border-left: hidden;
+ 			border-top: hidden;
+ 			border-right: hidden;
+ 			border-bottom: hidden;
+ 			margin-left: 350px;
+ 		}
+ 		.blue-button{
+ 			margin-left: 450px;
+ 		}
+ 	</style>
+ </head>
+ <body>
+ <section class="banner" id="top" style="background-image: url(imeges/cars.webp); width: 100%;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <div class="banner-caption">
+                        <div class="line-dec"></div>
+                        <form class="container">
+                        	<input type="" name="search" placeholder="Search Your Dream car">
+                        </form>
+                        <div class="blue-button">
+                            <a href="view cars.php">View All Cars </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+ </body>
+ </html>
+ 
